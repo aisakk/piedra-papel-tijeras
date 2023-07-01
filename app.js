3dfc24e0328a4139fc74477bbf4e4d0cfd11411b
Aisakk <santaellaisaac2@gmail.com>
2023-07-01 12:53:35 -0400

    Falta mas diseño, pero ya esta terminado

--- app.js
+++ app.js
@@ -1,40 +1,69 @@
 let papel = document.getElementById("papel");
 let piedra = document.getElementById("piedra");
 let tijera = document.getElementById("tijera");
-
+let winnerDOM = document.getElementById("winner");
+let ganador = document.getElementsByClassName("ganador")
 let machine = ["papel", "piedra", "tijera"];
+let winner = ["Maquina", "Usuario"]
 function choiceOption(option) {
     let random = Math.trunc(Math.random() * 3);
     let choice = machine[random];
-    console.log(option)
+
     if(option === "papel" && choice === "papel"){
-        return alert("Empate")
+       let texto = document.createTextNode("La maquina escogio Papel, esto es un Empate");
+       winnerDOM.innerHTML = "";
+       winnerDOM.appendChild(texto) 
+       winnerDOM.appendChild(papel.cloneNode(true)) 
     }
     if(option === "papel" && choice === "tijera"){
-        return alert("Maquina: tijera, ¡Gano Tijeras!")
+        let texto = document.createTextNode("La maquina escogio Tijera, ¡Perdiste!");
+        winnerDOM.innerHTML = "";
+       winnerDOM.appendChild(texto) 
+       winnerDOM.appendChild(papel.cloneNode(true)) 
     }
     if(option === "papel" && choice === "piedra"){
-        return alert("Maquina: piedra, ¡Gano Papel Felcitaciones!")
+        let texto = document.createTextNode("La maquina escogio Piedra, ¡Felicitaciones Ganaste!");
+        winnerDOM.innerHTML = "";
+        winnerDOM.appendChild(texto) 
+        winnerDOM.appendChild(papel.cloneNode(true)) 
     }
     
     if(option === "piedra" && choice === "piedra"){
-        return alert("Empate")
+        let texto = document.createTextNode("La maquina escogio Piedra, esto es un Empate");
+        winnerDOM.innerHTML = "";
+        winnerDOM.appendChild(texto) 
+        winnerDOM.appendChild(piedra.cloneNode(true)) 
     }
     if(option === "piedra" && choice === "tijera"){
-        return alert("Maquina: tijera, ¡Gano Piedras Felicitaciones!")
+        let texto = document.createTextNode("La maquina escogio Tijera, ¡Felicitaciones Ganaste!");
+        winnerDOM.innerHTML = "";
+        winnerDOM.appendChild(texto) 
+        winnerDOM.appendChild(piedra.cloneNode(true)) 
     }
     if(option === "piedra" && choice === "papel"){
-        return alert("Maquina: papel, ¡Gano Papel!")
+        let texto = document.createTextNode("La maquina escogio Papel, ¡Perdiste!");
+        winnerDOM.innerHTML = "";
+        winnerDOM.appendChild(texto) 
+        winnerDOM.appendChild(piedra.cloneNode(true)) 
     }
 
     if(option === "tijera" && choice === "tijera"){
-        return alert("Empate")
+        let texto = document.createTextNode("La maquina escogio Tijera, esto es un Empate");
+        winnerDOM.innerHTML = "";
+        winnerDOM.appendChild(texto) 
+        winnerDOM.appendChild(tijera.cloneNode(true)) 
     }
     if(option === "tijera" && choice === "papel"){
-        return alert("Maquina: tijera, ¡Gano Tijeras Felicitaciones!")
+        let texto = document.createTextNode("La maquina escogio Papel, ¡Felicitaciones Ganaste!");
+        winnerDOM.innerHTML = "";
+        winnerDOM.appendChild(texto) 
+        winnerDOM.appendChild(tijera.cloneNode(true))
     }
     if(option === "tijera" && choice === "piedra"){
-        return alert("Maquina: piedra, ¡Gano piedra!")
+        let texto = document.createTextNode("La maquina escogio Piedra, ¡Perdiste!");
+        winnerDOM.innerHTML = "";
+        winnerDOM.appendChild(texto) 
+        winnerDOM.appendChild(tijera.cloneNode(true))
     }
     
     //return random;
